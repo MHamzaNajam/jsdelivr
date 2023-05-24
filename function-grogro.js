a27820d5d6bf0d3b4a60c788509364837b51b4a9
Muhammad Hamza Najam <79852455+MHamzaNajam@users.noreply.github.com>
2023-05-24 13:52:01 +0500

    Update function-grogro.js

--- function-grogro.js
+++ function-grogro.js
@@ -1,17 +1,1 @@
 //javascript file for Ecwid store "grogro.no/products"
-//testing code for grogro.no/products
-
-import { Fireworks } from 'https://esm.run/fireworks-js';
-
-let container = document.querySelector('#ecwid_body');
-
-let options = {
-  gravity: 1.4,
-  opacity: 0.4,
-  autoresize: true,
-  acceleration: 1.00,
-};
-
-let fireworks = new Fireworks(container, options);
-
-fireworks.start();
